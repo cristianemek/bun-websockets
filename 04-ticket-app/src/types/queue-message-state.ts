ed7e654bb788f57bcb61070b32f0b317b47ece05
Cristian <cristianrodriguezlugo@gmail.com>
2026-02-09 09:49:32 +0100

    feat: agregar servicio y lógica para gestión de tickets en la cola

---
+++ 04-ticket-app/src/types/queue-message-state.ts
@@ -0,0 +1,18 @@
+import type { Ticket } from "./ticket";
+
+
+export interface QueueMessageState {
+    lastTicketNumbers: {
+        A: number;
+        P: number;
+    };
+    pending: {
+        normal: number;
+        preferential: number;
+        combined: number;
+    }
+    activeByDesk: Record<number, Ticket | undefined>;
+    recentlyServed: Ticket[];
+}
+
+
